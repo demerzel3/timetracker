b7912ec251b215f1551d379b20b8cf2d8e5a0cd9
Gabriele Genta <gabriele.genta@gmail.com>
2013-11-27 07:05:32 +0100

    Beginning the client structure.

---
+++ web/timetracker.dart
@@ -0,0 +1,90 @@
+import 'dart:html';
+import 'package:angular/angular.dart';
+import 'package:perf_api/perf_api.dart';
+
+class Timing {
+  String user;
+  DateTime date;
+  double duration;
+}
+
+class Task {
+  String name;
+  List<Timing> timings = new List<Timing>();
+  
+  Task(this.name);
+}
+
+class Project {
+  String name;
+  List<Task> tasks = new List<Task>();
+  
+  Project(this.name);
+}
+
+@NgController(
+    selector: '[index-controller]',
+    publishAs: 'ctrl')
+class IndexController {
+
+  List<Project> projects;
+  String newProjectName;
+    
+  Project selectedProject;
+  
+  String newTaskName;
+  Task selectedTask;
+  
+  IndexController(Scope scope, Http http) {
+    print(scope);
+    print(http);
+    
+    Project banco = new Project("Banco Farmaceutico");
+    banco.tasks.add(new Task("Velocizzare addVolontario"));
+    
+    projects = new List<Project>();
+    projects.add(banco);
+  }
+  
+  createNewProject() {
+    print("new project name: " + newProjectName);
+    if (newProjectName.length > 0) {
+      projects.add(new Project(newProjectName));
+      newProjectName = "";
+    }
+  }
+  
+  createNewTask() {
+    if (newTaskName.length > 0) {
+      selectedProject.tasks.add(new Task(newTaskName));
+      newTaskName = "";
+    }
+  }
+  
+  selectProject(Project project) {
+    selectedProject = project;
+    if (project.tasks.length > 0) {
+      selectedTask = project.tasks[0];
+    } else {
+      selectedTask = null;
+    }
+  }
+  
+  selectTask(Task task) {
+    selectedTask = task;
+  }
+  
+}
+
+class TimeTrackerModule extends Module {
+  TimeTrackerModule() {
+    type(IndexController);
+    //type(InlineEditableValue);
+    //type(Profiler, implementedBy: Profiler); // comment out to enable profiling
+  }
+}
+
+void main() {
+  // bootstrap angular
+  ngBootstrap(module: new TimeTrackerModule());
+}
